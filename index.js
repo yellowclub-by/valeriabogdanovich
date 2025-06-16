23b921f6ed2a66b7cc4306f5e7986b8e027f0351
yellowclub-by <165181476+yellowclub-by@users.noreply.github.com>
2025-06-16 17:22:41 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('Happy developing ✨')
+let username = "456"
+let password = "123"
+function login() {
+    let username_input = document.getElementById("username").value
+    let password_input = document.getElementById("password").value
+    if (username_input == username && password_input == password) {
+        alert("Вход выполнен, авторизация успешна!")
+        window.location.href = "../лерака/лерка.html"
+    } else {
+        alert("Неверный логин или пароль")
+    }
+    }
