c1223fcea0b510fafba56c2101d57c4b15f2be15
yellowclub-by <165181476+yellowclub-by@users.noreply.github.com>
2025-06-16 20:25:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function login() {
     let password_input = document.getElementById("password").value
     if (username_input == username && password_input == password) {
         alert("Вход выполнен, авторизация успешна!")
-        window.location.href = "лерака/лерка.html"
+        window.location.href = "lera/lera.html"
     } else {
         alert("Неверный логин или пароль")
     }
